55314462d42e19f8563e627fb4a332ee24025b11
Felipe Massera <felipemassera88@gmail.com>
2023-03-29 16:51:02 -0300

    subo ejercicios tp2,ej1,ej2,ej3,ej4 funcionando OK

--- Practicos/practica2/ejercicio3/p2ej3.pas
+++ Practicos/practica2/ejercicio3/p2ej3.pas
@@ -18,107 +18,145 @@ Const
 Type 
   producto = Record
     cod : Integer;
-    nombre: nombres;
+    nombre: string[30];
     descripcion : String [50];
     stockDis : integer;
     stockMin : integer;
     precio : double;
   End;
   producto_detalle =record
-    cod = Integer;
-    cantVendida = integer;
+    cod : Integer;
+    cantVendida : integer;
   end;
   maestro = file of producto;
   detalle = file of producto_detalle;
   sucursales = array [1..sucu] of detalle;
   registros = array [1..sucu] of  producto_detalle;
+   
+procedure informarB (var mae: maestro);
+var
+  auxMaestro: producto;
+begin
+  writeln('========================================[Informe]========================================');
+  Reset(mae);
+  while not eof(mae) do begin
+    read(mae, auxMaestro);
+    if (auxMaestro.stockMin > auxMaestro.stockDis) then begin
+      with auxMaestro do begin
+        writeln('----------------------------------------------------------------------------------------------------------');
+        writeln('Codigo: ', cod , ' Nombre: ', nombre, ' Descripccion:', ' Stock Disponible: ', stockDis, ' Stock Minimo ', stockMin, ' Precio', precio:2:2);
+        writeln('----------------------------------------------------------------------------------------------------------');
+      end;
+    end;
+  end;
+  Close(mae);
+end;
 
 procedure leer (var det : detalle; var aux : producto_detalle);
 begin
-  if not eof (det) then
-    read(det, aux)
-  else
+  if not eof (det) then begin
+    read(det, aux);
+  end else
     aux.cod:= valor_alto;
 end;
 
-procedure actualizarMaestro(Var mae: maestro; Var det: detalle );
+procedure minimo (var datos: registros; var min:producto_detalle; var archDet: sucursales);
 var
-  auxMae: producto;
-  auxDet: producto_detalle;
+  i, pos:integer;
 begin
-  assign(mae,'maestro');
-  Assign(det,'detalle');
-  reset(mae);
-  reset(det);
-  leer(det,auxDet);
-  while auxDet.cod<>valor_alto do begin
-    Read(mae, auxMae);
-    While auxMae.cod <> auxDet.cod Do Read(mae, auxMae);
-    while auxMae.cod = auxDet.cod do begin
-      auxMae.stockDis = auxMae.stockDis - auxDet.cantVendida;
-      leer(det,auxDet);
+  min.cod := valor_alto;
+  for i:= 1 to 30 do begin
+    if(datos[i].cod < min.cod) then begin
+      pos:=i;
+      min:= datos[i];
     end;
-      seek(mae, FilePos(mae)-1);
-      write(mae, auxMae);  
   end;
-  Close(mae);
-  Close(det);
-end;
-
-procedure descargoDiarios(var mae: maestro; suc : sucursales);
-var
-  i :integer;
-begin
-  for i:=0 to sucu do begin
-    actualizarMaestro(mae,suc[i]);
-  end;
-end;
-
-procedure cargoArray(var detDiarios:sucursales);
-var
-  aux : producto_detalle;
-  det : detalle;
-  arch : text;
-begin
-  assign(arch,'detalle.txt');
-  assign(det,'detalle');
-  Reset(Arch);
-  Rewrite(det);
-  while not eof (arch)do begin
-    read(arch, aux.cod, aux.cantVendida);
-    write(det,aux);
-  end;
-  //El array para la prueba contiene 30 listados =
-  for i:=1 to suc do
-    detDiarios[i]:= det;
-  close(arch);
-  close(det);
-end;
-
-procedure maximo (var archivo);
-var
-
-begin
-  
+  if (min.cod <> valor_alto) THEN 
+    leer(archDet[pos], datos[pos]);
 end;
 
 
+procedure actualizarMaestro(Var mae: maestro; var vectorSucursales: sucursales);
 var
-  arrayD : sucursales;
-  arrayR: registros;
+  auxMaestro: producto;
   min: producto_detalle;
-  nombreFisico:String;
-  i:integer;
-Begin
-  cargoArray(arrayD);
-
-  for i:=1 to suc do
-  begin
-    Str(i, iString);
-    assign(arrayD[i], 'detalle'+iString);
-    rewrite(arrayD[i]);
-    leer(arrayD[i], arrayR[i]);
+  vectorDetalles: registros;
+  nombre: string;
+  iString:string[10];
+  i: Integer;
+begin
+  for i:= 1 to 30 do begin
+    str(i,iString);
+    nombre := ('detalle1 - copia ('+ iString+')');
+    Assign(vectorSucursales[i], nombre);
+    Reset(vectorSucursales[i]);
+    Leer(vectorSucursales[i] , vectorDetalles[i]);
   end;
+  Reset(mae);
+  minimo(vectorDetalles, min, vectorSucursales);
+  while (min.cod <> valor_alto) do begin
+    read(mae, auxMaestro);
+    while (auxMaestro.cod <> min.cod) do read(mae, auxMaestro);
+    while (auxMaestro.cod = min.cod) do begin
+      auxMaestro.stockDis := auxMaestro.stockDis - min.cantVendida;
+      minimo(vectorDetalles, min, vectorSucursales);
+    end;
+    //WRITELN('Datos aux a escribir:',auxMaestro.nombre, auxMaestro.stockDis);
+    seek(mae, filepos(mae) -1 );
+    write(mae,auxMaestro);
+  end;
+  close(mae);
+  for i:= 1 to 30 do close(vectorSucursales[i]);
+  Writeln('Actualizacion realizada con exito');
+end;
 
-  minimo(arrayD, arrayR, min);
+PROCEDURE creoMaestro(var mae:maestro);
+var
+  auxM: producto;
+  txt : text;
+begin
+  Assign(txt,'maestro.txt');
+  Assign(mae,'maestro');
+  reset(txt);
+  Rewrite(mae);
+  while not eof(txt) do begin
+    with auxM do begin
+      readln(txt, cod, nombre);
+      readln(txt, stockDis, stockMin, descripcion);
+      readln(txt, precio);
+    end;
+    write(mae, auxM);
+  end;
+  WriteLn('Maestro creado con exito');
+end;
+procedure crearDetalle(); 
+  var
+    txt: text;
+    det: detalle;
+    auxD: producto_detalle;
+    nombre: String[30]; 
+    iString: String[30]; 
+  begin
+      assign(txt, 'detalle.txt');
+      reset(txt);
+      nombre := ('detalle1 - copia (1)');
+      assign(det,nombre);
+      Rewrite(det);
+      while not eof (txt)do begin
+        read(txt, auxD.cod, auxD.cantVendida);
+        write(det,auxD);
+      end;
+      close(det);
+    close(txt);
+    WriteLn('detalle creado con exito');
+  end;
+//para que ande el programa luego de crearel archivo detalle copiarlo 30 veces y cambiar los numeros de 1..30
+var
+  mae:maestro;
+  arrayDet: sucursales;
+Begin
+  creoMaestro(mae);
+  //crearArrayDetalles(arrayDet);
+  actualizarMaestro(mae,arrayDet);
+  informarB(mae);
 End.
