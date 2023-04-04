1ada334a98df2a1497f3e39698a342b448a68fcf
Felipe Massera <felipemassera88@gmail.com>
2023-04-04 14:14:15 -0300

    fin tp2 ej 5

--- Practicos/practica2/ejercicio5/tp2ej5.pas
+++ Practicos/practica2/ejercicio5/tp2ej5.pas
@@ -25,7 +25,7 @@ no tiene sentido}
 
 program  tp2ej5;
 const
-    df = 50;
+    df = 3;
     valor_alto= 9999;
 type
     strini= String[30];
@@ -41,7 +41,7 @@ type
         nombre : strini;
         apellido: strini;
         direccion : dire;
-        mat_medico: integer;
+        mat_medico_nac: integer;
         nombre_madre: strini;
         apellido_madre: strini;
         dni_madre: integer;
@@ -80,8 +80,11 @@ type
     det_nac= file of reg_nac;
     det_fal= file of reg_fallecido;
     mae_bin= file of reg_maestro;
-    Vec_nac = array [1..df] of reg_nac;
-    Vec_fal = array [1..df] of reg_fallecido;
+    Vec_nac = array [1..df] of det_nac;
+    Vec_fal = array [1..df] of det_fal;
+    nacimientos=array[1..df]of reg_nac;
+    fallecimientos=array[1..df]of reg_fallecido;
+    
 
  procedure leerN(var det : det_nac; var aux: reg_nac);
   begin
@@ -95,23 +98,27 @@ type
         read(det,aux)
       else aux.nro_partida:= valor_alto;  
  end;
+
+
  procedure verMaestro(var mae:mae_bin);
         procedure datos(aux: reg_maestro);
         begin
+          WriteLn('°°°°°-----------Datos Personales-----------°°°°°');
+
           WriteLn('Reg Nro',aux.nro_partida);
           Writeln(' Nombre: ',aux.nombre,' Apellido: ',aux.apellido, ' Dni: ',aux.DNI);
           WriteLn(' Direccion: ',aux.direccion.calle,' NRO: ',aux.direccion.nro);
           WriteLn(' PISO: ',aux.direccion.piso);
           WriteLn(' dTO: ',aux.direccion.depto);
           WriteLn(' Ciudad: ',aux.direccion.ciudad);
-          WriteLn('-----------Datos de Nacimiento-----------');
+          WriteLn('-----Datos de Nacimiento------');
           WriteLn(' Matricula medico del acta de nacimiento',aux.mat_medico_nac);
           WriteLn(' Nombre Madre: ',aux.nombre_madre,' Apellido madre: ',aux.dni_madre,' DNI Madre: ',aux.dni_madre);
           WriteLn(' Nombre padre: ',aux.nombre_padre,' Apellido Padre: ',aux.apellido_padre,' DNI Padre: ',aux.dni_padre);
           if (aux.mat_medico_fal <> 0) then
           begin
-            writeLn('---------------Si fallecio-------------- ');
-            writeln(' Matricula Medico del acta de fallecimiento: 'aux.mat_medico_fal);
+            writeLn('---------Si fallecio-------- ');
+            writeln(' Matricula Medico del acta de fallecimiento: ',aux.mat_medico_fal);
              writeln(' Fecha Fallecimiento: ',aux.fecha_fallece,' Hora: ',aux.hora_deceso,' donde Fallece: ',aux.lugar_deceso);
           end else WriteLn('No fallecio');
         end;
@@ -123,12 +130,176 @@ type
         read(mae, aux);
         datos(aux);
       end;
+      close(mae);
+ end;
+ procedure minimoNac(var vD: Vec_nac;var vN:nacimientos; var minimo: reg_nac);
+ var
+  pos,i:integer;
+  begin
+    minimo.nro_partida := valor_alto;
+    pos :=0;
+    for i:=1 to df do begin
+      if vN[i].nro_partida<minimo.nro_partida then
+      begin
+        pos:=i;
+        minimo:= vN[i];
+      end;
+    if(minimo.nro_partida<>valor_alto)then
+      leerN(vD[pos],vN[pos]);
+    end;
+  end;
+ procedure minimoFal(var vD: Vec_fal;var vF:fallecimientos; var minimo: reg_fallecido);
+ var
+  pos,i:integer;
+  begin
+    minimo.nro_partida := valor_alto;
+    pos := 0;
+    for i:=1 to df do begin
+      if (vF[i].nro_partida<minimo.nro_partida) then
+      begin
+        pos:=i;
+        minimo:= vF[i];
+      end;
+    if(minimo.nro_partida<>valor_alto)then
+      leerF(vD[pos],vF[pos]);
+    end;
+  end;
+
+procedure crearMaestro(var m:mae_bin;var aNac:Vec_nac;var aFal:Vec_fal;var naci:nacimientos;var falle:fallecimientos);
+  procedure cargarNyF( n: reg_nac; f:reg_fallecido; var m:reg_maestro);
+  begin
+    m.nro_partida:=n.nro_partida;
+    m.nombre:= n.nombre;
+    m.apellido:= n.apellido;
+    m.direccion.calle:= n.direccion.calle;
+    m.direccion.nro:= n.direccion.nro;
+    m.direccion.piso:= n.direccion.piso;
+    m.direccion.depto:= n.direccion.depto;
+    m.direccion.ciudad:= n.direccion.ciudad;
+    m.nombre_madre:= n.nombre_madre;
+    m.apellido_madre:= n.apellido_madre;
+    m.nombre_padre:= n.nombre_padre;
+    m.apellido_padre:= n.apellido_padre;
+    m.dni_madre:=n.dni_madre;
+    m.dni_padre:= n.dni_padre;
+    m.mat_medico_nac:= n.mat_medico_nac;
+    { aca arrancan fallecimientos}
+    m.DNI:= f.DNI;
+    m.mat_medico_fal:= f.mat_medico;
+    m.fecha_fallece:= f.fecha_fallece;
+    m.hora_deceso:= f.hora_deceso;
+    m.lugar_deceso:= f.lugar_deceso;
+  end;
+  procedure cargarN( n: reg_nac; var m:reg_maestro);
+  begin
+    m.nro_partida:=n.nro_partida;
+    m.nombre:= n.nombre;
+    m.apellido:= n.apellido;
+    m.direccion.calle:= n.direccion.calle;
+    m.direccion.nro:= n.direccion.nro;
+    m.direccion.piso:= n.direccion.piso;
+    m.direccion.depto:= n.direccion.depto;
+    m.direccion.ciudad:= n.direccion.ciudad;
+    m.nombre_madre:= n.nombre_madre;
+    m.apellido_madre:= n.apellido_madre;
+    m.nombre_padre:= n.nombre_padre;
+    m.apellido_padre:= n.apellido_padre;
+    m.dni_madre:=n.dni_madre;
+    m.dni_padre:= n.dni_padre;
+    m.mat_medico_nac:= n.mat_medico_nac;
+    { nota: se pone en 0 la matricula del medico que declara el fallecimientopara usarlo como boolean cuando mostramosMaestro}
+    m.mat_medico_fal:= 0;
+  end;
+var
+  minNaci:reg_nac;
+  minFal:reg_fallecido;
+  regMae:reg_maestro;
+  i:integer;
+begin
+  assign(m,'maestro');
+  rewrite(m);
+  minimoNac(aNac,naci,minNaci);
+  minimoFal(aFal,falle,minFal);
+  while(minNaci.nro_partida<>valor_alto)do begin
+    if(minNaci.nro_partida=minFal.nro_partida)then begin
+      cargarNyF(minNaci,minFal,regMae);
+      minimoNac(aNac,naci,minNaci);
+      minimoFal(aFal,falle,minFal);
+    end
+    else begin 
+      cargarN(minNaci,regMae);
+      minimoNac(aNac,naci,minNaci);
+    end;
+    write(m,regMae);
+  end;
+  close(m);
+  for i:=1 to df do begin
+    close(aNac[i]);
+    close(aFal[i]);
+  end;
+end;
+
+ procedure asignarDetalles(var aNac : Vec_nac;var aFal: Vec_fal;var naci:nacimientos;var falle:fallecimientos);
+ var
+  i:integer;
+  nombreN,nombreF,iString:string;
+  detN:det_nac;
+  detF:det_fal;
+ begin
+  for i:=1 to df do begin
+    str(i,iString);
+    nombreN:= 'Nac ('+istring+')';
+    nombreF:= 'Fal ('+istring+')';
+    Assign(detN,nombreN);
+    Assign(detF,nombreF);
+    reset(detN);
+    reset(detF);
+    aNac[i]:=detN;
+    aFal[i]:=detF;
+    leerN(aNac[i],naci[i]);
+    leerF(aFal[i],falle[i]);
+  end;
  end;
 
- procedure 
+
+procedure exportar(var mae:mae_bin);
+var
+  texto:text;
+  regMae:reg_maestro;
+begin
+  assign(texto,'maestro.txt');
+  rewrite(texto);
+  reset(mae);
+  while(not eof(mae))do begin
+    read(mae,regMae);
+    with regMae do begin
+      writeln(texto,nro_partida,' ',DNI,' ',mat_medico_nac,' ',dni_madre,' ',dni_padre,' ',direccion.calle,' ',direccion.piso,' ',direccion.depto,' ',direccion.ciudad);
+      writeln(texto,nombre);
+      writeln(texto,apellido);
+      writeln(texto,nombre_madre);
+      writeln(texto,apellido_madre);
+      writeln(texto,nombre_padre);
+      writeln(texto,apellido_padre);
+      writeln(texto,mat_medico_fal,' ',fecha_fallece);
+      writeln(texto,hora_deceso);
+      writeln(texto,lugar_deceso);
+    end;
+  end;
+  close(mae);
+  close(texto);
+end;
 
 var
-
+  aNac:Vec_nac;
+  aFal : Vec_fal;
+  naci:nacimientos;
+  falle:fallecimientos;
+  mae : mae_bin;
 begin
-  
+  asignarDetalles(aNac,aFal,naci,falle);
+  crearMaestro(mae,aNac,aFal,naci,falle);
+  verMaestro(mae);
+  exportar(mae);
 End.
+
+{Archivo creado por Gonzalez Ivan y Massera Felipe}
