62aa3f907ad7ad649c7a17b81c5182c0277856f5
Felipe Massera <felipemassera88@gmail.com>
2023-07-17 11:17:48 -0300

    subo info de material de promo teorica

--- Parciales/PracticaParcial2023/ParArchivos2020/ejercicio2/ej2.pas
+++
@@ -1,9 +0,0 @@
-{    2. Se desea modelar un sistema para manejar las ventas realizadas en una cadena de repuestos para autos.
- La cadena cuenta con 3 sucursales y una casa matriz. Todas las semanas la casa matriz recibe un archivo binario
-  de cada sucursal con el detalle de las ventas realizadas. Cada archivo cuenta con la siguiente
-   información: cod_repuesto, nombre_repuesto, cantidad_vendida y fechaYhora_venta. Cada repuesto en 
-   los detalles puede no aparecer o aparecer en más una o varias ventas y todos los archivos están ordenados por cod_repuesto.
-Escriba un programa (Programa principal, estructuras y módulos) que dados los archivos detalle de cada sucursal,
- genere un nuevo archivo binario llamado total_repuestos_vendidos.dat con la suma de ventas de cada repuesto en todas
-  las sucursales. El nuevo archivo deberá tener el siguiente formato: cod_repuesto, nombre_repuesto y cantidad_total_vendida.
-Nota: los archivos deben recorrerse solo una vez.} 
